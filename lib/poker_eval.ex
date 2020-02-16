01b6a8106b7939f7adfc2284783d15df03c93727
Kaio Ferreira <kaiolf11@gmail.com>
2020-02-16 19:14:48 -0300

    initial commit

---
+++ lib/poker_eval.ex
@@ -0,0 +1,18 @@
+defmodule PokerEval do
+  @moduledoc """
+  Documentation for PokerEval.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PokerEval.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poker_eval_test.exs
@@ -0,0 +1,8 @@
+defmodule PokerEvalTest do
+  use ExUnit.Case
+  doctest PokerEval
+
+  test "greets the world" do
+    assert PokerEval.hello() == :world
+  end
+end
