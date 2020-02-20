ce487518844d2947c65b9e92fcc4bde34be020c5
Kaio Ferreira <kaiolf11@gmail.com>
2020-02-20 08:49:09 -0300

    test: add rank cases to be tested

--- test/poker_eval/rank_test.exs
+++ test/poker_eval/rank_test.exs
@@ -1,4 +1,64 @@
 defmodule PokerEval.RankTest do
-  use ExUnit.TestCase
+  use ExUnit.Case
   doctest PokerEval.Rank
+
+  alias PokerEval.Rank
+
+  describe "rank hands" do
+
+    @tag :pending
+    test "evaluates a straight flush" do
+      hand = []
+      {:straight_flush, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a four of a kind" do
+      hand = []
+      {:four_of_a_kind, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a full_house" do
+      hand = []
+      {:full_house, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a flush" do
+      hand = []
+      {:flush, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a straight" do
+      hand = []
+      {:straight, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a three_of_a_kind" do
+      hand = []
+      {:three_of_a_kind, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a two_pairs" do
+      hand = []
+      {:two_pairs, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a pair" do
+      hand = []
+      {:pair, _} = Rank.rank_hand(hand)
+    end
+
+    @tag :pending
+    test "evaluates a high_card" do
+      hand = []
+      {:high_card, _} = Rank.rank_hand(hand)
+    end
+
+  end
 end
