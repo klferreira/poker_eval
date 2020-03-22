2423962190b7c758d19820bcbb57e1fd8a650d05
Kaio Ferreira <kaiolf11@gmail.com>
2020-03-22 13:56:05 -0300

    test: add four of a kind tests

--- test/poker_eval/rank_test.exs
+++ test/poker_eval/rank_test.exs
@@ -2,62 +2,33 @@ defmodule PokerEval.RankTest do
   use ExUnit.Case
   doctest PokerEval.Rank
 
+  alias PokerEval.Card
   alias PokerEval.Rank
 
-  describe "rank hands" do
+  describe "get_four_of_a_kind" do
 
-    @tag :pending
-    test "evaluates a straight flush" do
-      hand = []
-      {:straight_flush, _} = Rank.rank_hand(hand)
+    # @tag :pending
+    test "returns nil if hand doesn't match rank" do
+      cards = [
+        %Card{rank: 2, suit: "H"},
+        %Card{rank: 7, suit: "S"},
+        %Card{rank: 2, suit: "D"},
+        %Card{rank: 3, suit: "C"},
+        %Card{rank: 10, suit: "H"}
+      ]
+      nil = Rank.four_of_a_kind?(cards)
     end
 
-    @tag :pending
-    test "evaluates a four of a kind" do
-      hand = []
-      {:four_of_a_kind, _} = Rank.rank_hand(hand)
-    end
-
-    @tag :pending
-    test "evaluates a full_house" do
-      hand = []
-      {:full_house, _} = Rank.rank_hand(hand)
-    end
-
-    @tag :pending
-    test "evaluates a flush" do
-      hand = []
-      {:flush, _} = Rank.rank_hand(hand)
-    end
-
-    @tag :pending
-    test "evaluates a straight" do
-      hand = []
-      {:straight, _} = Rank.rank_hand(hand)
-    end
-
-    @tag :pending
-    test "evaluates a three_of_a_kind" do
-      hand = []
-      {:three_of_a_kind, _} = Rank.rank_hand(hand)
-    end
-
-    @tag :pending
-    test "evaluates a two_pairs" do
-      hand = []
-      {:two_pairs, _} = Rank.rank_hand(hand)
-    end
-
-    @tag :pending
-    test "evaluates a pair" do
-      hand = []
-      {:pair, _} = Rank.rank_hand(hand)
-    end
-
-    @tag :pending
-    test "evaluates a high_card" do
-      hand = []
-      {:high_card, _} = Rank.rank_hand(hand)
+    # @tag :pending
+    test "returns :four_of_a_kind when hand matches rank" do
+      cards = [
+        %Card{rank: 2, suit: "H"},
+        %Card{rank: 5, suit: "S"},
+        %Card{rank: 5, suit: "D"},
+        %Card{rank: 5, suit: "C"},
+        %Card{rank: 5, suit: "H"}
+      ]
+      {:four_of_a_kind, 5} = Rank.four_of_a_kind?(cards)
     end
 
   end
