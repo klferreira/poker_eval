fbc4a85b19e78003d94cd0db72e79801eeed5818
Kaio Ferreira <kaiolf11@gmail.com>
2020-10-14 23:37:12 -0300

    feat: add hand module

---
+++ lib/poker/hand.ex
@@ -0,0 +1,17 @@
+defmodule Poker.Hand do
+  import PokerEval.Rank
+
+  defstruct cards: [], rank: nil
+
+  def get_rank(hand, community) do
+    rank = rank_with(hand ++ community, get_ordered_ranks())
+    {:ok, rank, hand}
+  end
+
+  def rank_with(hand, [rank | ranks]) do
+    case apply(PokerEval.Rank, :"#{rank}?", [hand]) do
+      {rank, _} -> rank
+      _ -> rank_with(hand, ranks)
+    end
+  end
+end
