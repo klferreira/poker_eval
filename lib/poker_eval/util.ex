3a92aad8a86f74dec37d195467ed44ca1a177288
Kaio Ferreira <kaiolf11@gmail.com>
2020-10-14 23:44:21 -0300

    chore: full_deck returning card instances

--- lib/poker_eval/util.ex
+++ lib/poker_eval/util.ex
@@ -1,6 +1,11 @@
 defmodule PokerEval.Util do
 
+  alias PokerEval.Card
+
   def full_deck() do
-    for rank <- ~w(2 3 4 5 6 7 8 9 T J Q K A), suit <- ~w(C D H S), do: rank <> suit
+    for rank <- ~w(2 3 4 5 6 7 8 9 T J Q K A), suit <- ~w(C D H S) do
+      {:ok, card} = Card.of(rank <> suit)
+      card
+    end
   end
 end
