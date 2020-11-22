5fd525af017ce84c797c6a406ce6cfd4f6873602
Kaio Ferreira <kaiolf11@gmail.com>
2020-11-22 18:30:02 -0300

    chore: improve dealer module tests

--- test/poker/dealer_test.exs
+++ test/poker/dealer_test.exs
@@ -5,31 +5,32 @@ defmodule Poker.DealerTest do
   alias Poker.Card
   alias Poker.Dealer
 
-  @full_deck_size 52
-
   describe "new" do
-    test "creates a new dealer instance with a fresh deck" do
-      dealer = Dealer.new()
-      @full_deck_size = length(dealer.deck)
+    setup do
+      {:ok, dealer: Dealer.new()}
+    end
+
+    test "creates a new dealer instance with a fresh deck", context do
+      assert 52 == length(context[:dealer].deck)
     end
   end
 
   describe "deal_hole_cards/2" do
-    test "deals n starting hands and keeps the remaining cards" do
+    setup do
       player_count = Enum.random(2..5)
       {:ok, hands, dealer} = Dealer.deal_hole_cards(Dealer.new(), player_count)
-      @full_deck_size = length(dealer.deck) + length(hands) * 2
+      {:ok, hands: hands, dealer: dealer}
+    end
+
+    test "dealt cards are removed from deck", context do
+      assert 52 == length(context[:dealer].deck) + length(context[:hands]) * 2
     end
   end
 
   describe "deal_community_cards/1" do
     setup do
       cards = [
-        Card.of("KS"),
-        Card.of("2D"),
-        Card.of("2C"),
-        Card.of("5S"),
-        Card.of("QH")
+        Card.of("KS"), Card.of("2D"), Card.of("2C"), Card.of("5S"), Card.of("QH")
       ]
 
       {:ok, cards: cards}
@@ -40,29 +41,31 @@ defmodule Poker.DealerTest do
       3 = length(dealer.community_cards)
     end
 
-    test "deals a single card if community card pool has 3 or 4 cards", context do
-      with dealer <-
-             Enum.take(context[:cards], 3)
-             |> Dealer.new()
-             |> Dealer.deal_community_cards() do
-        4 = length(dealer.community_cards)
-      end
+    test "deals a single card if community card pool has 3 cards", context do
+      dealer =
+        Enum.take(context[:cards], 3)
+        |> Dealer.new()
+        |> Dealer.deal_community_cards()
 
-      with dealer <-
-             Enum.take(context[:cards], 4)
-             |> Dealer.new()
-             |> Dealer.deal_community_cards() do
-        5 = length(dealer.community_cards)
-      end
+      assert 4 == length(dealer.community_cards)
+    end
+
+    test "deals a single card if community card pool has 4 cards", context do
+      dealer =
+        Enum.take(context[:cards], 4)
+        |> Dealer.new()
+        |> Dealer.deal_community_cards()
+
+      assert 5 == length(dealer.community_cards)
     end
 
     test "does not deal if community card pool is full", context do
-      with dealer <-
-             Enum.take(context[:cards], 5)
-             |> Dealer.new()
-             |> Dealer.deal_community_cards() do
-        5 = length(dealer.community_cards)
-      end
+      dealer =
+        Enum.take(context[:cards], 5)
+        |> Dealer.new()
+        |> Dealer.deal_community_cards()
+
+      assert 5 == length(dealer.community_cards)
     end
   end
 end
